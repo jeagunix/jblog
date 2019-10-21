d44b2ee6315de4ed26d2444ae14025366c819fcd
JeaGunix <dlwprn1541@gmail.com>
2019-10-21 17:26:58 +0900

    jblog3,4,5 pom.xml 파일 수정

---
+++ jblog6/src/main/java/kr/co/itcen/jblog/repository/UserDao.java
@@ -0,0 +1,33 @@
+package kr.co.itcen.jblog.repository;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import kr.co.itcen.jblog.vo.UserVo;
+
+
+@Repository
+public class UserDao {
+
+	@Autowired
+	private SqlSession sqlSession;
+
+
+	public void insert(UserVo vo)  { //왜 boolean으로 할까? 반환
+		sqlSession.insert("user.insert", vo);
+	}
+
+	public UserVo getId(String id) {
+		UserVo result = sqlSession.selectOne("user.getById", id);
+		return result;
+	}
+
+	public UserVo getUserImfo(UserVo vo) {
+		UserVo result = sqlSession.selectOne("user.getUserImfo", vo);
+		return result;
+	}
+
+
+
+}
---
+++ jblog6/src/main/java/kr/co/itcen/jblog/service/UserService.java
@@ -0,0 +1,30 @@
+package kr.co.itcen.jblog.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.co.itcen.jblog.repository.UserDao;
+import kr.co.itcen.jblog.vo.UserVo;
+
+
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserDao userDao;
+
+	public void join(UserVo vo) {
+		userDao.insert(vo);
+	}
+
+	public Boolean existUser(String id) {
+		return userDao.getId(id) != null;
+	}
+
+	public UserVo getUserImfo(UserVo vo) {
+		return userDao.getUserImfo(vo);
+	}
+
+	
+}
